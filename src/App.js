2467acbbc571833fd6a19642faacef869ed6dc15
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-09-25 15:50:30 +0300

    upd hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Stepan",
+      age: 25,
+      show: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ name: "Mykola", age: "30", show: !this.state.show });
+  };
+
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        {this.state.show && (
+          <h1>
+            name: {this.state.name}, age: {this.state.age}
+          </h1>
+        )}
+        <button
+          style={{ padding: "10px 20px" }}
+          onClick={this.handleClick}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {this.state.show ? "Скрыть" : "Показать"}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
