eed2790c1a87d6619156d912914759ca744b907f
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-10-04 16:22:36 +0300

    hw 3

--- src/App.js
+++ src/App.js
@@ -1,34 +1,73 @@
 import "./App.css";
 import React from "react";
+import Client from "./components/Client";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "Stepan",
-      age: 25,
-      show: false,
-    };
+  state = { clients: [] };
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((clients) => this.setState({ clients: clients.slice(0, 10) }));
   }
-
-  handleClick = () => {
-    this.setState({ name: "Mykola", age: "30", show: !this.state.show });
+  updateHandler = async (id, value) => {
+    try {
+      const result = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}`,
+        {
+          method: "PATCH",
+          body: JSON.stringify({
+            title: `${value}`,
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+      result.status === 200 &&
+        this.setState({
+          clients: this.state.clients.map((client) =>
+            client.id === id ? { ...client, title: value } : client
+          ),
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  removeHandler = async (id) => {
+    try {
+      const result = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      result.status === 200 &&
+        this.setState({
+          clients: this.state.clients.filter((client) => client.id !== id),
+        });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   render() {
     return (
-      <div style={{ textAlign: "center" }}>
-        {this.state.show && (
-          <h1>
-            name: {this.state.name}, age: {this.state.age}
-          </h1>
-        )}
-        <button
-          style={{ padding: "10px 20px" }}
-          onClick={this.handleClick}
-        >
-          {this.state.show ? "Скрыть" : "Показать"}
-        </button>
+      <div
+        style={{
+          display: "flex",
+          flexFlow: "column nowrap",
+          padding: "15px",
+          gap: "10px",
+        }}
+      >
+        {this.state.clients.map((client) => (
+          <Client
+            client={client}
+            updateHandler={this.updateHandler}
+            removeHandler={this.removeHandler}
+            key={client.id}
+          />
+        ))}
       </div>
     );
   }
---
+++ src/components/Client.jsx
@@ -0,0 +1,43 @@
+import React, { PureComponent } from "react";
+
+export class Client extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+  componentWillUnmount() {
+    alert("Операция выполнена успешно");
+  }
+  componentDidUpdate() {
+    alert("Операция выполнена успешно");
+  }
+  render() {
+    return (
+      <div style={{ display: "flex", gap: "10px" }}>
+        <input
+          style={{ flex: "1", border: "none", borderBottom: "1px solid black" }}
+          defaultValue={this.props.client.title}
+          ref={this.myRef}
+        />
+        <button
+          onClick={() =>
+            this.props.updateHandler(
+              this.props.client.id,
+              this.myRef.current.value
+            )
+          }
+        >
+          Update
+        </button>
+        <p style={{ flexBasis: "20%", textAlign: "center" }}>
+          User id is:{this.props.client.id}
+        </p>
+        <button onClick={() => this.props.removeHandler(this.props.client.id)}>
+          delete
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Client;
