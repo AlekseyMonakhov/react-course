65e0e26d3f63309ed540baa7f14290857b75c5b5
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-11-26 16:36:14 +0200

    upd hw10

--- hw6/src/pages/Popular/Popular.jsx
+++ hw6/src/pages/Popular/Popular.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Repo from "../../components/Repo/Repo";
 import SelectedLang from "../../components/SelectedLang/SelectedLang";
-import "./popular.css";
 
 const Popular = () => {
     return (
--- hw6/src/redux/store.js
+++ hw6/src/redux/store.js
@@ -8,6 +8,7 @@ import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
     key: 'root',
+    version: 1,
     storage,
 }
 
