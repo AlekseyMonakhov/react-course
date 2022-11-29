1e49adc7aa435fd95d071fba34e175aba162605c
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-11-29 02:13:34 +0200

    ts config upd

--- hw10TypeScript/src/index.tsx
+++ hw10TypeScript/src/index.tsx
@@ -6,7 +6,7 @@ import {persistor, store} from "./redux/store";
 import {PersistGate} from 'redux-persist/integration/react'
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
