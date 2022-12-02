cf1266bf7f8aba19e53ff23c2d1037d6e054358f
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-12-02 03:04:00 +0200

    upd for mob resolution

--- hw10TypeScript/src/pages/Home.tsx
+++ hw10TypeScript/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 const Home = () => {
     return (
         <div className='home-container'>
-            <h1>GitHub Battle: Battle your friends... and stuff.</h1>
+            <h1>GitHub Battle: <br/>Battle your friends... and stuff.</h1>
             <Link
                 className='button'
                 to='/battle'
