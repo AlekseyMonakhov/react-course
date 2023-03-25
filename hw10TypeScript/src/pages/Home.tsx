892e45c526754715fef9c1555d21bb412906b874
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-03-25 22:31:01 +0200

    upd

--- hw10TypeScript/src/pages/Home.tsx
+++ hw10TypeScript/src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div className='home-container'>
-            <h1>GitHub Battle: <br/>Battle your friends... and stuff.</h1>
-            <Link
-                className='button'
-                to='/battle'
-            >
+        <div className={"home-container"}>
+            <h1>
+                GitHub Battle: <br />
+                Battle your friends... and stuff.
+            </h1>
+            <Link className="button" to="/battle">
                 Battle
             </Link>
         </div>
